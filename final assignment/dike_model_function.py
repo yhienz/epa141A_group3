21cbf2228904611a2bcb37a2fc1cc0cec0567699
MLKragtwijk <marloeskragtwijk@gmail.com>
2024-05-28 11:33:55 +0200

    marloes test

--- final assignment/dike_model_function.py
+++ final assignment/dike_model_function.py
@@ -15,6 +15,8 @@ from funs_dikes import Lookuplin, dikefailure, init_node
 from funs_economy import cost_fun, discount, cost_evacuation
 from funs_hydrostat import werklijn_cdf, werklijn_inv
 
+#hallo hallo marloes hiero
+
 
 def Muskingum(C1, C2, C3, Qn0_t1, Qn0_t0, Qn1_t0):
     """Simulates hydrological routing"""
